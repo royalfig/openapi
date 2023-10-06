fa7edcf127900810c6ce36ddca575512b522a187
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2023-10-06 17:07:12 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import fs from 'fs/promises';
 
 const inputFiles = [];
 
 fs.readdir('public').then((files) => {
     for (const file of files) {
-        if (!file.endsWith('.html')) {continue;}
-        console.log("🚀 ~ file: vite.config.js:8 ~ fs.readdir ~ file:", file)
+        if (!file.endsWith('.html')) {
+            continue;
+        }
         inputFiles.push(`public/${file}`);
     }
 });
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: inputFiles,
-    },
-  },
+    build: {
+        rollupOptions: {
+            input: inputFiles
+        }
+    }
 });
